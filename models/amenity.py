576031609e2798904e2dbc152276ce1536cdfe60
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-06-24 18:44:05 +0000

    Amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-
-""" Amenity Module for HBNB project """
-
-import os
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-
-    """Represents an amenity data set."""
-
-    __tablename__ = 'amenities'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    """ class Amenity inherits from BaseModel and Base """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
+    # name = ""
