16365d61fd4abd7c0fe88a086be0f1f5e8112824
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-06-25 08:20:31 -0700

    User errors corrected

--- models/user.py
+++ models/user.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+"""Defines the User class."""
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+class User(BaseModel):
+    """Represent a User.
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
 
-    places = relationship('Place', cascade='all, delete', backref='user')
-    reviews = relationship('Review', cascade='all, delete', backref='user')
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
