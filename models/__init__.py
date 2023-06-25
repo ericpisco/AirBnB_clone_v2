61fcf3b26026840dc57e6c1cea908a107dbcce8a
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-06-25 06:56:39 -0700

    Models

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-
 """This module instantiates an object of class FileStorage"""
+from os import getenv
 
-import os
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
 
-from models.engine.db_storage import DBStorage
-from models.engine.file_storage import FileStorage
-
-storage = DBStorage() if os.getenv(
-    'HBNB_TYPE_STORAGE') == 'db' else FileStorage()
-
-"""A unique FileStorage/DBStorage instance for all models.
-"""
-
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,27 +1,34 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
 import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship, backref
 from models.city import City
-import os
+from os import getenv
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="delete", backref="state")
 
-    if os.getenv('HBNB_TYPE_STORAGE') != "db":
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete", backref="state")
+
+    else:
+        name = ""
+
         @property
         def cities(self):
-            """ cities getter attribute """
-            cit_lis = []
-            all_cit = models.storage.all(City)
-            for city in all_cit.values():  # change .items() to values() as it
-                # returns an obj that contains values of a dictionary as a list
-                if city.state_id == self.id:
-                    cit_lis.append(city)
-            return cit_lis
+            """ Returns the list of City
+            """
+            the_cities = []
+            for c in models.storage.all(City).values():
+                if c.state_id == self.id:
+                    the_cities.append(c)
+            return the_cities
